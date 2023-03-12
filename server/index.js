f36d7c50624df6ded0df8607f8d0459e96fe077d
hongjolim <hongjolim@geotab.com>
2023-03-11 16:25:40 -0800

    Set up mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 import express from 'express';
 
+import userRouter from './routers/users.js';
+import authRouter from './routers/auth.js';
+import blogRouter from './routers/blogs.js';
+
+import mongoose from 'mongoose';
+
 const PORT = 5000;
-const HOST = 'http://localhost';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello to the server side!");
-})
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/blogs', blogRouter);
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-});
+const CONNECTION_URL = 'mongodb+srv://hongjolim:%40Ghdwh0326@cluster0.axfkv1w.mongodb.net/ketogram?retryWrites=true&w=majority';
+
+mongoose.connect(CONNECTION_URL)
+.then(
+    app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
+}))
+.catch(err => {
+    if(err){
+        console.log(err);
+    }
+})
