aa838d1617ebe1d9ae2652b849af26971a9a181e
hongjolim <hongjolim@geotab.com>
2023-04-05 15:40:09 -0700

    Added posts and sidebar sections

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import "./Header.css";
 import MainImg from '../../images/main.jpg';
 
-export default function Header() {
+const Header = () => {
   return (
     <div className="header">
       <img className="headerImg" src={MainImg} alt="" />
     </div>
   );
 }
+
+export default Header;
