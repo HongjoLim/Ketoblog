b8696e02a65d750e47943aa11816e5d44d6d798b
hongjolim <hongjolim@geotab.com>
2023-04-08 10:49:03 -0700

    Completed blogs crud testing

--- server/controllers/blogs.js
+++ server/controllers/blogs.js
@@ -4,13 +4,12 @@ import jwt from "jsonwebtoken";
 export const getBlogs = async (req, res) => {
     const user_email = req.query.email;
     const cat = req.query.cat;
-
-    try{
+    try {
 
         let blogs;
-        
-        if(user_email) {
-            blogs = await Blog.find({user_email});
+
+        if (user_email) {
+            blogs = await Blog.find({ user_email });
         } else if (cat) {
             blogs = await Blog.find({
                 categories: {
@@ -21,39 +20,48 @@ export const getBlogs = async (req, res) => {
             blogs = await Blog.find();
         }
 
-    res.status(200).send(blogs);
-    } catch(err) {
+        res.status(200).json(blogs);
+    } catch (err) {
         res.status(500).json(err);
     }
 }
 
 export const getBlog = async (req, res) => {
-    const blog = await Blog.findById(req.params._id);
-    res.status(200).json(blog);
+    try {
+        const blog = await Blog.findById(req.params._id);
+        res.status(200).json(blog);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 }
 
 export const addBlog = async (req, res) => {
     const token = req.cookies.access_token;
-    
+
     if (!token) {
         console.log('token not exists');
         res.status(401).json("Not authenticated!");
     }
 
     jwt.verify(token, "jwtkey", (err, userInfo) => {
-    if (err) { 
+        if (err) {
             res.status(403).json("Invalid access token");
-    }});
-
-    const blog = new Blog({
-        title: req.body.title,
-        desc: req.body.content,
-        img: req.body.img_url,
-        cats: req.body.cats,
-        user_email: req.body.user_email
+        }
     });
-    await blog.save();
-    res.status(200).send('Added');
+
+    try {
+        const blog = new Blog({
+            title: req.body.title,
+            desc: req.body.desc,
+            img: req.body.img_url,
+            cats: req.body.cats,
+            user_email: req.body.user_email
+        });
+        await blog.save();
+        res.status(200).json(blog);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 }
 
 export const deleteBlog = (req, res) => {
@@ -61,15 +69,19 @@ export const deleteBlog = (req, res) => {
     const token = req.cookies.access_token;
     if (!token) {
         return res.status(401).json('Not authenticated!');
-    } else 
-    jwt.verify(token, 'jwtkey', async (err, userInfo) => {
-        if (err){
-            res.status(403).json('Invalid access token');
-        } else {
-            await Blog.findByIdAndDelete(req.params._id);
-            res.status(200).send('deleted');
-        }
-    });
+    } else
+        jwt.verify(token, 'jwtkey', async (err, userInfo) => {
+            if (err) {
+                res.status(403).json('Invalid access token');
+            } else {
+                try {
+                    await Blog.findByIdAndDelete(req.params._id);
+                    res.status(200).json('Blog post has been deleted');
+                } catch (err) {
+                    res.status(500).json(err);
+                }
+            }
+        });
 }
 
 export const updateBlog = async (req, res) => {
@@ -77,11 +89,16 @@ export const updateBlog = async (req, res) => {
     if (!token) {
         return res.status(401).json('Authentication failed!');
     } else jwt.verify(token, 'jwtkey', async (err, userInfo) => {
-        if (err){
+        if (err) {
             res.status(403).json('Invalid access token');
         } else {
-            await Blog.updateOne({_id: req.params._id}, {title: req.body.title, content: req.body.content, cat:req.body.cat, img_url:req.body.img_url});
-            res.status(200).send('updated');
+            try {
+                const post = await Blog.findOneAndUpdate({ _id: req.params._id },
+                    { $set: req.body }, { new: true });
+                res.status(200).json(post);
+            } catch (err) {
+                res.status(500).json(err);
+            }
         }
     });
 }
