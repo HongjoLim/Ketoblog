8d6e663fceaf541f8c20700c2bc9e8c74e7ceabc
hongjolim <hongjolim@geotab.com>
2023-04-08 13:21:51 -0700

    bug fix

--- client/src/components/Topbar/Topbar.jsx
+++ client/src/components/Topbar/Topbar.jsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
 import { Link } from "react-router-dom";
-import Logo from '../../images/logo.png';
 import { AuthContext } from '../../context/authContext';
 import './Topbar.css';
 
