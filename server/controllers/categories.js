d4661223991ca05e354a973457393f734addf7d9
hongjolim <hongjolim@geotab.com>
2023-04-08 10:24:12 -0700

    Finished category crud

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -1,10 +1,47 @@
 import Category from '../models/category.js';
 
 export const getCategories = async (req, res) => {
-    try{
+    try {
         const cats = await Category.find();
-        res.status(200).send(cats);
-    }catch (err) {
-        res.status(404).send('cannot find data');
+        res.status(200).json(cats);
+    } catch (err) {
+        res.status(404).json('cannot find data');
+    }
+};
+
+export const addCategory = async (req, res) => {
+    try {
+        const cat = await Category.findOne({ name: req.body.name });
+
+        if (cat) {
+            res.status(409).json('Category already exists!')
+        } else {
+            new Category({ name: req.body.name }).save();
+            res.status(200).json('Category added!');
+
+        }
+    } catch (err) {
+        res.status(500).json('Cannot add category!');
+    }
+};
+
+export const updateCategory = async (req, res) => {
+    try {
+        await Category.findOneAndUpdate({
+            _id: req.params._id
+        }, { $set: req.body },
+            { new: true });
+        res.status(200).json('Category updated!');
+    } catch (err) {
+        res.status(404).json('Cannot update category!');
+    }
+};
+
+export const deleteCategory = async (req, res) => {
+    try {
+        await Category.findByIdAndDelete(req.params._id);
+        res.status(200).json('Category deleted!');
+    } catch (err) {
+        res.status(500).json('Cannot delete category!');
     }
 };
--- server/routers/categories.js
+++ server/routers/categories.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getCategories } from '../controllers/categories.js';
+import { getCategories, addCategory, updateCategory, deleteCategory } from '../controllers/categories.js';
 
 const categoryRouter = express.Router();
 
 categoryRouter.get('/', getCategories);
+categoryRouter.post('/', addCategory);
+categoryRouter.put('/:_id', updateCategory);
+categoryRouter.delete('/:_id', deleteCategory);
 
 export default categoryRouter;
